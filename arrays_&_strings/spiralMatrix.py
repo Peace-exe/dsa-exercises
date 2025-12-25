b248785e0547bb260044c476f4a519640d3bb06a
Aman Yadav <amanyadav2303@gmail.com>
2025-12-26 04:17:35 +0530

    two best non overlapping events - lc 2056

---
+++ arrays_&_strings/spiralMatrix.py
@@ -0,0 +1,20 @@
+def spiralOrder(matrix: list[list[int]])->list[int]:
+
+        res=[]
+        m = len(matrix[0])
+        n = len(matrix)
+
+        
+        return res
+        
+print(spiralOrder( [[1,2,3,4],[5,6,7,8],[9,10,11,12]]))
+
+'''
+  n
+m [1,2,3,4]
+  [5,6,7,8]
+  [5,6,7,8]
+  [5,6,7,8]
+  [9,10,11,12]
+
+'''
--- arrays_&_strings/two_best_non_overlapping.py
+++ arrays_&_strings/two_best_non_overlapping.py
@@ -1,10 +1,46 @@
 def maxTwoEvents(events : list[list[int]])->int:
 
-    if not events:
-        return 0
-    events.sort(key= lambda interval:interval[0] )
-    for i in range(len(events)):
-        print(events[i])
+        if not events:
+            return 0
+        events.sort(key= lambda interval:interval[0] )
+        maxSum = 0
+        validInterval=-1
+
+        suffixMax = [0]*len(events)
+        suffixMax[-1]=events[-1][2]
+
+        for i in range(len(events)-2,-1,-1):
+              suffixMax[i]=max(events[i][2], suffixMax[i+1])
+
+        for i in range(len(events)):
+            
+            validInterval=-1
+
+            if(maxSum<events[i][2]):
+                    maxSum=events[i][2]
+
+
+            low= i+1
+            high = len(events)-1
+            while low<=high:
+                    mid = (low+high)//2
+
+                    #going left
+                    if events[mid][0]> events[i][1]:
+                          validInterval=mid
+                          high = mid-1
+                    #going right 
+                    if events[mid][0]<=events[i][1]:
+                          low = mid+1
+            
+            if validInterval>-1:
+                  maxSum = max(maxSum, events[i][2] + suffixMax[validInterval])
+            
+            
+                      
+        return maxSum                 
+        
+        
         
 
-maxTwoEvents([[1,3,2],[4,5,2],[2,4,3]])
+print(maxTwoEvents([[1,5,3],[1,5,1],[6,6,5]]))
