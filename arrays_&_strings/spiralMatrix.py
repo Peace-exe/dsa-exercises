061b7ce6c4d4ca1a83f223c1fe885fffeb8406e8
Aman Yadav <amanyadav2303@gmail.com>
2025-12-27 04:35:41 +0530

    spiral order of matrix - leetcode 54

--- arrays_&_strings/spiralMatrix.py
+++ arrays_&_strings/spiralMatrix.py
@@ -1,9 +1,54 @@
 def spiralOrder(matrix: list[list[int]])->list[int]:
 
         res=[]
-        m = len(matrix[0])
-        n = len(matrix)
+        n = len(matrix[0])
+        m = len(matrix)
+        
+        upWall = 0 
+        rightWall = len(matrix[0])
+        downWall = len(matrix)
+        leftWall = -1
 
+        UP,RIGHT,DOWN,LEFT=0,1,2,3
+        DIRECTION = RIGHT
+
+        i,j=0,0
+
+
+        while len(res) != m*n:
+
+            if DIRECTION==RIGHT:
+                  while j<rightWall:
+                        res.append(matrix[i][j])
+                        j+=1
+                  rightWall-=1
+                  i+=1
+                  j-=1
+                  DIRECTION=DOWN
+            elif DIRECTION==DOWN:
+                  while i<downWall:
+                        res.append(matrix[i][j])
+                        i+=1
+                  downWall-=1
+                  i-=1
+                  j-=1
+                  DIRECTION=LEFT
+            elif DIRECTION==LEFT:
+                  while j>leftWall:
+                        res.append(matrix[i][j])
+                        j-=1
+                  leftWall+=1
+                  i-=1
+                  j+=1
+                  DIRECTION=UP
+            else:
+                  while i> upWall:
+                        res.append(matrix[i][j])
+                        i-=1
+                  upWall+=1
+                  i+=1
+                  j+=1
+                  DIRECTION=RIGHT
         
         return res
         
